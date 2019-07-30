b8b95a3af6ef3bb83dea2c70fda0de0fe29a7432
hamzadz-123 <53497206+hamzadz-123@users.noreply.github.com>
2019-07-31 00:36:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("حط الاي دي حق الروم اللي يحصل بيها الاسبام هنا")
+let channel =     client.channels.get("605890970797473903")
 setInterval(function() {
-channel.send(`حط الكلام اللي تبغاه هنا`);
+channel.send(`تم دعس من قبل حمزة حمزة حمزة حمزة حمزة حمزة حمزة حمزة حمزة حمزة حمزة حمزة`);
 }, 30)
 })
 
